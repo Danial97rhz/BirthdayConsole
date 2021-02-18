9501824ff0361ce68b7a1ff3825dc20e533e66c3
Rahim Zadeh Danial (TREADLER) <danial.rahimzadeh@hm.com>
2021-02-18 11:11:18 +0100

    add sleepy writeLines

--- Program.cs
+++ Program.cs
@@ -9,10 +9,7 @@ namespace HappyBirthday
         {
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Green;
-            for (int i = 0; i < 10; i++)
-            {
             Console.WriteLine("Happy Birthday Ajanta!!!");
-            }
             Grasshoper();
             Console.ForegroundColor = ConsoleColor.Gray;
         }
@@ -22,6 +19,7 @@ namespace HappyBirthday
             Thread.Sleep(2000);
             Console.Beep(264, 125);
             Thread.Sleep(250);
+            Console.WriteLine("Happy Birthday Ajanta!!!");
             Console.Beep(264, 125);
             Thread.Sleep(125);
             Console.Beep(297, 500);
@@ -30,46 +28,54 @@ namespace HappyBirthday
             Thread.Sleep(125);
             Console.Beep(352, 500);
             Thread.Sleep(125);
+            Console.WriteLine("Happy Birthday Ajanta!!!");
             Console.Beep(330, 1000);
             Thread.Sleep(250);
             Console.Beep(264, 125);
             Thread.Sleep(250);
             Console.Beep(264, 125);
             Thread.Sleep(125);
+            Console.WriteLine("Happy Birthday Ajanta!!!");
             Console.Beep(297, 500);
             Thread.Sleep(125);
             Console.Beep(264, 500);
             Thread.Sleep(125);
+            Console.WriteLine("Happy Birthday Ajanta!!!");
             Console.Beep(396, 500);
             Thread.Sleep(125);
             Console.Beep(352, 1000);
             Thread.Sleep(250);
             Console.Beep(264, 125);
             Thread.Sleep(250);
+            Console.WriteLine("Happy Birthday Ajanta!!!");
             Console.Beep(264, 125);
             Thread.Sleep(125);
             Console.Beep(2642, 500);
             Thread.Sleep(125);
             Console.Beep(440, 500);
             Thread.Sleep(125);
+            Console.WriteLine("Happy Birthday Ajanta!!!");
             Console.Beep(352, 250);
             Thread.Sleep(125);
             Console.Beep(352, 125);
             Thread.Sleep(125);
             Console.Beep(330, 500);
             Thread.Sleep(125);
+            Console.WriteLine("Happy Birthday Ajanta!!!");
             Console.Beep(297, 1000);
             Thread.Sleep(250);
             Console.Beep(466, 125);
             Thread.Sleep(250);
             Console.Beep(466, 125);
             Thread.Sleep(125);
+            Console.WriteLine("Happy Birthday Ajanta!!!");
             Console.Beep(440, 500);
             Thread.Sleep(125);
             Console.Beep(352, 500);
             Thread.Sleep(125);
             Console.Beep(396, 500);
             Thread.Sleep(125);
+            Console.WriteLine("Happy Birthday Ajanta!!!");
             Console.Beep(352, 1000);
         }
     }
